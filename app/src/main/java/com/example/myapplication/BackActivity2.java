ee2e01ef15b72d401155706be13aebab52ada60c
DaniilShkarovskiy <72796379+DaniilShkarovskiy@users.noreply.github.com>
2020-12-22 15:22:32 +0300

    screen

---
+++ app/src/main/java/com/example/myapplication/BackActivity2.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class BackActivity2 extends AppCompatActivity {
+
+    private Button btn;
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back2);
+
+ }
+
+
+
+
+
+
+    }
